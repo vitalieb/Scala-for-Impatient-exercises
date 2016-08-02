d12e7e17c512b23d67198a783972ff1635431f6f
Vitalie Bordiniuc <Vitalie.Bordiniuc@4finance.com>
2016-08-02 16:41:47 +0300

    Removed unnecessary code

--- ch3/ex2.scala
+++ ch3/ex2.scala
@@ -1,12 +1,10 @@
-
-
 //Write a loop that swaps adjacent elements of an array of integers. For example, Array(1, 2, 3, 4,
 //  5) becomes Array(2, 1, 4, 3, 5).
 
 
 def swapAdjacent(array: Array[Int]) = {
   for (i <- array.indices if i%2==0) {
-    var temp = array(i)
+    val temp = array(i)
     if (i + 1 < array.length) {
       array(i) = array(i + 1)
       array(i+1) = temp
