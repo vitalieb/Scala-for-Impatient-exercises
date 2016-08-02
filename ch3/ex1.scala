7fffc88df8a11d7fcbfd232c2567d53888e8199f
Vitalie Bordiniuc <Vitalie.Bordiniuc@4finance.com>
2016-08-02 13:57:33 +0300

    Added comments

--- ch3/ex1.scala
+++ ch3/ex1.scala
@@ -1,3 +1,6 @@
+//1. Write a code snippet that sets a to an array of n random integers between 0 (inclusive) and n
+//(exclusive).
+
 def generateRandom(n:Int):Array[Int]={
   var a:Array[Int] = (for(i<-0 until n) yield scala.util.Random.nextInt(n-1)).toArray
   a
